a0c8ae4b6ed7192c00cfe8b36f44639c5ee94a01
Heber Aguilera <heber.aguilera1@gmail.com>
2026-07-08 20:01:36 -0600

    inicializadas las variables y matrices

--- 09_Matrices/operacionesBasicasMatrices.cpp
+++ 09_Matrices/operacionesBasicasMatrices.cpp
@@ -6,15 +6,21 @@
     EL REPORTE DE LOS RESULTADOS DEBE MOSTRARSE EN PANTALLA Y GUARDARSE EN UN ARCHIVO DE TEXTO
   */
 
-  #include <iostream>
+  #include <iostream> 
   #include <fstream> // Librería para manejo de archivos
     using namespace std;
+
+//Definir e inicializar las matrices
+int filas, columnas;
+
+double datos1[10][10];
+double datos2[10][10];
 
     int main() {
 
 
 
 
-        
+
         return 0;
     }   

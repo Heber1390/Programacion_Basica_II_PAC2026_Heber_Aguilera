28c07c0f641f387beda0073058f77e6757442b81
Heber Aguilera <heber.aguilera1@gmail.com>
2026-07-08 19:51:40 -0600

    ejercicios iniciales con matrices

---
+++ 09_Matrices/operacionesBasicasMatrices.cpp
@@ -0,0 +1,20 @@
+/* Hacer un programa en el cual a partir de un menu con las 4 operaciones basicas
+  (1. suma, 2. resta, 3. multiplicacion, 4. division)
+  se ingresen diferentes valores en una matriz (CUADRADADA) definida por el usuario 
+    NO MAYOR A 10 FILAS Y 10 COLUMNAS)
+  
+    EL REPORTE DE LOS RESULTADOS DEBE MOSTRARSE EN PANTALLA Y GUARDARSE EN UN ARCHIVO DE TEXTO
+  */
+
+  #include <iostream>
+  #include <fstream> // Librería para manejo de archivos
+    using namespace std;
+
+    int main() {
+
+
+
+
+        
+        return 0;
+    }   
